2d56a5f48e1a23d422fa9e1032ab2e1e8b43d919
Dasbadman <tom-trumper+dasbadman@hotmail.co.uk>
2025-08-15 13:47:27 +0100

    Add High Elf to racemasks for Hunter and Warrior weapon skills

---
+++ dist/IndividualProgressionPatch/zz_highelf_individual_prog.sql
@@ -0,0 +1,5 @@
+UPDATE `playercreateinfo_skills` SET `racemask` = 2568 WHERE `racemask` = 520 AND `classMask` = 4 AND `skill` = 173; -- Hunter - Daggers
+
+UPDATE `playercreateinfo_skills` SET `racemask` = 3255 WHERE `racemask` = 1207 AND `classMask` = 1 AND `skill` = 44; -- Warrior - Axes
+UPDATE `playercreateinfo_skills` SET `racemask` = 3693 WHERE `racemask` = 1645 AND `classMask` = 1 AND `skill` = 54; -- Warrior - Maces
+UPDATE `playercreateinfo_skills` SET `racemask` = 3675 WHERE `racemask` = 1627 AND `classMask` = 1 AND `skill` = 43; -- Warrior - Swords
